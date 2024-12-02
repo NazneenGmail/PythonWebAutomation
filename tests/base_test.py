64d5342bea1a33934270337c878be88fc19e344d
Nazneen Dadhiwale <NazneenGmail@users.noreply.github.com>
2024-12-02 15:57:32 -0800

    Update base_test.py
    Setting Jenkins to run tests on cloud platform

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,10 +1,10 @@
 import pytest
 
 
-@pytest.mark.usefixtures("initialize_driver")
-class BaseTest:
-    pass
-
-# @pytest.mark.usefixtures("driver_initialization")
+# @pytest.mark.usefixtures("initialize_driver")
 # class BaseTest:
 #     pass
+
+@pytest.mark.usefixtures("driver_initialization")
+class BaseTest:
+    pass
